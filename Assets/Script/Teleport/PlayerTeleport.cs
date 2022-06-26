0f5f2ef98edb16c9a4a2d348387c84ad0eaf1e21
CodingVirus <glakffkdldk1@gmail.com>
2022-06-26 13:13:43 +0900

    Update PlayerTeleport.cs

--- Assets/Script/Teleport/PlayerTeleport.cs
+++ Assets/Script/Teleport/PlayerTeleport.cs
@@ -10,13 +10,21 @@ public class PlayerTeleport : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 위 화살표 누르고 현재 currentTeleporter가 null이 아니라면 목적지 위치로 이동
+        // Invoke 기능을 사용하여 0.5초뒤에 이동
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             if (currentTeleporter != null)
             {
-                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+                Invoke("MovePosition", 0.5f);
+                
             }
         }
+    }
+
+    private void MovePosition()
+    {
+        transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
