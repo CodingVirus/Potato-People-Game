03fff3b50cc3099e82f16a7b84525565862d56f6
CodingVirus <glakffkdldk1@gmail.com>
2022-06-26 10:02:46 +0900

    텔레포트 중간 저장

--- Assets/Script/Teleport/PlayerTeleport.cs
+++ Assets/Script/Teleport/PlayerTeleport.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerTeleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    private GameObject currentTeleporter;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentTeleporter != null)
+            {
+                transform.position = currentTeleporter.GetComponent<Teleporter>().GetDestination().position;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            currentTeleporter = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Door"))
+        {
+            if (collision.gameObject == currentTeleporter)
+            {
+                currentTeleporter = null;
+            }
+        }
     }
 }
