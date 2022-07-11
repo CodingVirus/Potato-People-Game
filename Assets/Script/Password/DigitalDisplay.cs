75a1be8ab25db4ac36a4920bd7ee7bb6421f9d06
wawa9149 <wawa9149@naver.com>
2022-07-11 18:57:54 +0900

    password

---
+++ Assets/Script/Password/DigitalDisplay.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+
+public class DigitalDisplay : MonoBehaviour
+{
+    public static Action pw;
+
+    [SerializeField]
+    public Text codeText;
+
+    private string codeTextValue;
+    public CanvasGroup passwordgroup;
+
+    public void Click()
+    {
+        passwordgroup.alpha = 1;
+    }
+
+    private void Start() 
+    {
+        pw = () => {Click();};
+        codeTextValue = "";
+        passwordgroup.alpha = 0;
+    }
+
+    public void AddDigitToCodeTextValue(string digitEntered)
+    {
+        codeTextValue += digitEntered;
+        codeText.text = codeTextValue;
+        if(codeTextValue.Length > 4)
+        {
+            ResetDisplay();
+        }
+    }
+
+    public void CheckResults()
+    {
+        if(codeTextValue == "0921")
+        {
+            Debug.Log("Correct!");
+            passwordgroup.alpha = 0;
+        }
+        else
+        {
+            Debug.Log("Wrong!");
+            ResetDisplay();
+        }
+    }
+
+    public void ResetDisplay()
+    {
+        codeTextValue = "";
+        codeText.text = codeTextValue;
+    }
+
+    public void CloseDisplay()
+    {
+        ResetDisplay();
+        passwordgroup.alpha = 0;
+    }
+    
+}
