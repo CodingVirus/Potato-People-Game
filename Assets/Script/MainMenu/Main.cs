d06dd442cc9085cc2a58eaa85f8921ac7617c810
KNUkjy <fare1293@gmail.com>
2022-07-27 13:16:02 +0900

    d
    d

--- Assets/Script/MainMenu/Main.cs
+++ Assets/Script/MainMenu/Main.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class Main : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
