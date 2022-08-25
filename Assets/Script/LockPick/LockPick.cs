5c43ca65189ed640b9da2d2590c370923911eaf8
KNUkjy <fare1293@gmail.com>
2022-08-26 05:46:43 +0900

    자물쇠미니게임
    자물쇠임

---
+++ Assets/Script/LockPick/LockPick.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LockPick : MonoBehaviour
+{
+    public GameObject lockPick;
+    public GameObject lockPick_1;
+    public GameObject lockPick_2;
+    public GameObject lockPick_3;
+    public GameObject lockPick_4;
+    public GameObject lockPick_5;
+    public GameObject lockPick_6;
+    public GameObject lockPick_7;
+    public static int i ;
+    int Randomnum;
+
+    [SerializeField]
+    List<GameObject> LockPickList = new List<GameObject>();
+    public void LockPickAdd()
+    {
+        
+        LockPickList.Add(lockPick_1);
+        LockPickList.Add(lockPick_2);
+        LockPickList.Add(lockPick_3);
+        LockPickList.Add(lockPick_4);
+        LockPickList.Add(lockPick_5);
+        LockPickList.Add(lockPick_6);
+        LockPickList.Add(lockPick_7);
+        LockPickList.Add(lockPick);
+    }
+    public void LockPickActive()
+    {
+        
+        {
+            LockPickList[i].SetActive(true);
+            if (i >= 1)
+            {
+                LockPickList[i - 1].SetActive(false);
+            }
+            if (i == 0)
+            {
+                LockPickList[7].SetActive(false);
+            }
+            }
+    }
+    public void LockPickChange()
+    {
+        {
+            LockPickActive();
+            i++;
+
+            if (i >= 8)
+            {
+
+                i = 0;
+
+            }
+        }
+    }
+    public void UnLockCheck()
+    {
+
+        if(i == Randomnum)
+        {
+            Debug.Log("UnLock");
+        }
+        else
+        {
+            Debug.Log("Lock");
+        }
+       
+       
+        
+    }
+  
+
+    public void Start()
+    {
+        LockPickAdd();
+        Randomnum = Random.Range(0, 8);
+        Debug.Log(Randomnum);
+
+    }
+
+  
+    void Update()
+    {
+
+
+    }
+}
