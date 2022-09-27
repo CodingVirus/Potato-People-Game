846902a05381777173039e02ebd7e0e82ec877bc
CodingVirus <glakffkdldk1@gmail.com>
2022-09-27 17:14:49 +0900

    Update GameDataControl.cs

--- Assets/Script/GameData/GameDataControl.cs
+++ Assets/Script/GameData/GameDataControl.cs
@@ -6,6 +6,7 @@ public class GameDataControl : MonoBehaviour
 {
     public GameObject door3;
     public GameObject ply;
+
     public bool b3Door = false;
     public bool b2Door = false;
     public bool b1Door = false;
