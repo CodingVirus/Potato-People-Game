d0a9436b77cf763d680a6af2afeff465020f8cef
CodingVirus <glakffkdldk1@gmail.com>
2022-09-27 15:22:17 +0900

    ee

---
+++ Assets/Script/GameData/GameDataControl.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDataControl : MonoBehaviour
+{
+    public bool b3Door = false;
+    public bool b2Door = false;
+    public bool b1Door = false;
+}
