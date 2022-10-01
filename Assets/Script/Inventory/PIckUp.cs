2acaf95a3cedf4b1e7cf2f5c065130ca6d69d0cc
CodingVirus <glakffkdldk1@gmail.com>
2022-10-01 19:00:50 +0900

    ee

--- Assets/Script/Inventory/PIckUp.cs
+++ Assets/Script/Inventory/PIckUp.cs
@@ -51,50 +51,36 @@ public class PIckUp : MonoBehaviour
             //ply.GetComponent<PlayerMouseControll>().StartMove();
             active = false;
         }
+
+        else if (this.name == "Vending Machine" && collision.CompareTag("Player") && active == true)
+        {
+            ply = collision.gameObject;
+            Inventory inven = ply.GetComponent<Inventory>();
+            for (int i = 0; i < inven.slots.Count; i++)
+            {
+                if (inven.slots[i].isEmpty)
+                {
+                    Instantiate(slotItem, inven.slots[i].slotObj.transform, false);
+                    inven.slots[i].isEmpty = false;
+                    //this.GetComponent<BoxCollider2D>().enabled = false;
+                    break;
+                }
+            }
+            //ply.GetComponent<PlayerMouseControll>().target = ply.transform.position;
+            //ply.GetComponent<PlayerMouseControll>().StartMove();
+            active = false;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (this.name == "Vending Machine" && collision.CompareTag("Player") && active == true)
-        {
-            this.transform.GetChild(0).gameObject.SetActive(true);
-            ply = collision.gameObject;
-            ply.GetComponent<PlayerMouseControll>().StopMove();
-        }
-
-        else if (this.name == "Drug Combiner" && collision.CompareTag("Player") && active == true)
+        if (this.name == "Drug Combiner" && collision.CompareTag("Player") && active == true)
         {
             this.transform.GetChild(0).gameObject.SetActive(true);
             ply = collision.gameObject;
             this.transform.GetChild(0).GetChild(0).gameObject.GetComponent<DrugCombination>().drugCombinerPlayerObj = ply.gameObject;
             ply.GetComponent<PlayerMouseControll>().StopMove();
         }
-    }
-
-    public void PressYesButton()
-    {
-        Inventory inven = ply.GetComponent<Inventory>();
-        for (int i = 0; i < inven.slots.Count; i++)
-        {
-            if (inven.slots[i].isEmpty)
-            {
-                Instantiate(slotItem, inven.slots[i].slotObj.transform, false);
-                inven.slots[i].isEmpty = false;
-                this.transform.GetChild(0).gameObject.SetActive(false);
-                break;
-            }
-        }
-        ply.GetComponent<PlayerMouseControll>().target = ply.transform.position;
-        ply.GetComponent<PlayerMouseControll>().StartMove();
-        active = false;
-    }
-
-    public void PreesNoButton()
-    {
-        this.transform.GetChild(0).gameObject.SetActive(false);
-        active = false;
-        ply.GetComponent<PlayerMouseControll>().target = ply.transform.position;
-        ply.GetComponent<PlayerMouseControll>().StartMove();
     }
 
     private void OnMouseDown()
---
+++ Assets/Script/Item/GetItem.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetItem : MonoBehaviour
+{
+    float MaxDistance = 15f;
+    Vector3 MousePostion;
+    Camera Camera;
+    public GameObject ply;
+    public List<GameObject> ItemList = new List<GameObject>();
+
+    private void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MousePostion = Input.mousePosition;
+            MousePostion = Camera.ScreenToWorldPoint(MousePostion);
+
+            RaycastHit2D hit = Physics2D.Raycast(MousePostion, transform.forward, MaxDistance);
+            if (hit)
+            {
+                if (hit.transform.gameObject.tag == "Item")
+                {
+                    ply.GetComponent<PlayerMouseControll>().StopMove();
+                    Inventory inven = ply.GetComponent<Inventory>();
+                    for (int i = 0; i < inven.slots.Count; i++)
+                    {
+                        if (inven.slots[i].isEmpty)
+                        {
+                            Instantiate(ItemList[0], inven.slots[i].slotObj.transform, false);
+                            inven.slots[i].isEmpty = false;
+                            break;
+                        }
+                    }
+                    ply.GetComponent<PlayerMouseControll>().DialogueStartMove();
+                    hit.transform.GetComponent<BoxCollider2D>().enabled = false;
+                }
+
+                else if (hit.transform.gameObject.tag == "ClueyKey")
+                {
+                    Debug.Log("key!!!");
+                }
+            }
+        }
+    }
+}
