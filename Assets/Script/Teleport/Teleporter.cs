a87745dd0f5a6a8e44895753d67173e434d2a89a
wawa9149 <wawa9149@naver.com>
2022-11-10 18:48:07 +0900

    수정

--- Assets/Script/Teleport/Teleporter.cs
+++ Assets/Script/Teleport/Teleporter.cs
@@ -20,23 +20,5 @@ public class Teleporter : ClickablePortal
     public void DoctorTalkMove()
     {
         //OnMouseDown();
-    }
-
-    private void Update() 
-    {
-        // PlayerPosition.GetComponent<PlayerTeleport>().transferStart = true;
-        // Debug.Log("MouseDown : true");
-
-        // mousePos = Input.mousePosition;
-        // transPos = Camera.main.ScreenToWorldPoint(mousePos);
-        // target = new Vector3(transPos.x, target.y, 0);
-
-        // RaycastHit2D raycast = Physics2D.Raycast(PlayerPosition, target, 1, LayerMask.GetMask("Door"));
-        // if(Physics2D.Raycast(PlayerPosition, target, 1, LayerMask.GetMask("Door")))
-        // {
-        //     Debug.Log(raycast.collider.gameobject.name);
-        // }
-        // //Debug.Log(Rayhit.collider.gameobject.name);
-    }
-   
+    }   
 }
