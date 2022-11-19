d0cbe891ba0caaad57af68ca82b1e12c21ce756c
CodingVirus <glakffkdldk1@gmail.com>
2022-11-20 01:01:07 +0900

    ee

---
+++ Assets/Script/Player/DetectPlayer.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class DetectPlayer : MonoBehaviour
+{
+    //public GameObject ActiveButton;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            this.transform.GetChild(0).gameObject.SetActive(true);
+            //ActiveButton.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+            //ActiveButton.SetActive(false);
+        }
+    }
+}
